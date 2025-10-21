9ebc567153d4c643fbb731a763855d5d43971c23
xzhc <1838998336@qq.com>
2025-10-22 00:25:25 +0800

    Components

---
+++ my-practice/react-fundamentals/src/App.jsx
@@ -0,0 +1,12 @@
+import Greet from "./components/Greet";
+
+const App = () => {
+  return (
+    <>
+      <div>Component 🤝</div>
+      <Greet />
+    </>
+  );
+};
+
+export default App;
---
+++ my-practice/react-fundamentals/src/components/Greet.jsx
@@ -0,0 +1,9 @@
+// function Greet() {
+//   return <h1>Greet</h1>;
+// }
+
+const Greet = () => {
+  return <h1>Greet</h1>;
+};
+
+export default Greet;
